a2ed2da84c706f2aaf3590c62d9d2c2be322d1c3
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-02 19:30:49 +0530

    Add button filling in on click and removal from last button.

--- script.js
+++ script.js
@@ -11,21 +11,27 @@ img.src = images[0];
 const imageContainer = document.querySelector(".image-container");
 imageContainer.append(img);
 let index = 0; 
+let prev = 0;
 
 const clickCircle = (idx) => {
     index = idx;
     img.src = images[idx];
+    circleButtons[idx].classList.add("fill");
+    circleButtons[prev].classList.remove("fill");
+    prev = idx;
 }
 
 const n = images.length;
+const circleButtons = [];
 const circles = document.querySelector(".circles");
 for (let i = 0; i < n; i++) {
     const circle = document.createElement("div");
     circle.addEventListener("click", () => clickCircle(i));
     circle.classList.add("circle");
     circles.appendChild(circle);
+    circleButtons.push(circle);
 }
-
+circleButtons[0].classList.add("fill");
 
 const leftArrow = document.querySelector(".left-arrow");
 leftArrow.addEventListener("click", () => {
